7c810182e459e6187ed3a552f6868eea9c836759
Agent Isai <icastro@agentisai.com>
2015-10-11 18:52:33 -0500

    Twst

---
+++ modules/modules/nickserv/return.d
@@ -0,0 +1,260 @@
+return.so: return.c ../../include/atheme.h ../../include/sysconf.h \
+ ../../include/stdinc.h ../../libmowgli-2/src/libmowgli/mowgli.h \
+ ../../libmowgli-2/src/libmowgli/core/stdinc.h \
+ ../../libmowgli-2/src/libmowgli/platform/machine.h \
+ ../../libmowgli-2/src/libmowgli/platform/cacheline.h \
+ ../../libmowgli-2/src/libmowgli/platform/attributes.h \
+ ../../libmowgli-2/src/libmowgli/platform/constructor.h \
+ ../../libmowgli-2/src/libmowgli/core/logger.h \
+ ../../libmowgli-2/src/libmowgli/core/assert.h \
+ ../../libmowgli-2/src/libmowgli/core/iterator.h \
+ ../../libmowgli-2/src/libmowgli/container/list.h \
+ ../../libmowgli-2/src/libmowgli/object/class.h \
+ ../../libmowgli-2/src/libmowgli/object/object.h \
+ ../../libmowgli-2/src/libmowgli/core/allocation_policy.h \
+ ../../libmowgli-2/src/libmowgli/core/alloc.h \
+ ../../libmowgli-2/src/libmowgli/container/dictionary.h \
+ ../../libmowgli-2/src/libmowgli/thread/thread.h \
+ ../../libmowgli-2/src/libmowgli/thread/mutex.h \
+ ../../libmowgli-2/src/libmowgli/base/memslice.h \
+ ../../libmowgli-2/src/libmowgli/container/patricia.h \
+ ../../libmowgli-2/src/libmowgli/module/module.h \
+ ../../libmowgli-2/src/libmowgli/container/queue.h \
+ ../../libmowgli-2/src/libmowgli/base/hash.h \
+ ../../libmowgli-2/src/libmowgli/core/heap.h \
+ ../../libmowgli-2/src/libmowgli/core/bootstrap.h \
+ ../../libmowgli-2/src/libmowgli/base/bitvector.h \
+ ../../libmowgli-2/src/libmowgli/base/hook.h \
+ ../../libmowgli-2/src/libmowgli/base/mowgli_signal.h \
+ ../../libmowgli-2/src/libmowgli/ext/proctitle.h \
+ ../../libmowgli-2/src/libmowgli/ext/error_backtrace.h \
+ ../../libmowgli-2/src/libmowgli/base/random.h \
+ ../../libmowgli-2/src/libmowgli/base/argstack.h \
+ ../../libmowgli-2/src/libmowgli/object/message.h \
+ ../../libmowgli-2/src/libmowgli/object/metadata.h \
+ ../../libmowgli-2/src/libmowgli/ext/global_storage.h \
+ ../../libmowgli-2/src/libmowgli/core/process.h \
+ ../../libmowgli-2/src/libmowgli/eventloop/eventloop.h \
+ ../../libmowgli-2/src/libmowgli/vio/vio.h \
+ ../../libmowgli-2/src/libmowgli/core/mowgli_string.h \
+ ../../libmowgli-2/src/libmowgli/core/allocator.h \
+ ../../libmowgli-2/src/libmowgli/base/formatter.h \
+ ../../libmowgli-2/src/libmowgli/container/index.h \
+ ../../libmowgli-2/src/libmowgli/ext/confparse.h \
+ ../../libmowgli-2/src/libmowgli/ext/program_opts.h \
+ ../../libmowgli-2/src/libmowgli/ext/json.h \
+ ../../libmowgli-2/src/libmowgli/ext/json-inline.h \
+ ../../libmowgli-2/src/libmowgli/linebuf/linebuf.h \
+ ../../libmowgli-2/src/libmowgli/dns/dns.h \
+ ../../libmowgli-2/src/libmowgli/dns/evloop_res.h \
+ ../../libmowgli-2/src/libmowgli/dns/evloop_reslib.h ../../include/i18n.h \
+ ../../include/common.h ../../include/object.h ../../include/connection.h \
+ ../../include/res.h ../../include/atheme.h ../../include/reslib.h \
+ ../../include/hook.h ../../include/hooktypes.h \
+ ../../include/atheme_string.h ../../include/atheme_memory.h \
+ ../../include/table.h ../../include/servers.h ../../include/channels.h \
+ ../../include/module.h ../../include/privs.h ../../include/abirev.h \
+ ../../include/crypto.h ../../include/culture.h ../../include/base64.h \
+ ../../include/md5.h ../../include/sasl.h ../../include/match.h \
+ ../../include/account.h ../../include/entity.h \
+ ../../include/entity-validation.h ../../include/auth.h \
+ ../../include/tools.h ../../include/confprocess.h ../../include/global.h \
+ ../../include/flags.h ../../include/phandler.h \
+ ../../include/commandtree.h ../../include/servtree.h \
+ ../../include/services.h ../../include/users.h \
+ ../../include/sourceinfo.h ../../include/taint.h \
+ ../../include/database_backend.h ../../include/uid.h \
+ ../../include/inline/account.h ../../include/inline/channels.h \
+ ../../include/inline/connection.h ../../include/authcookie.h
+
+../../include/atheme.h:
+
+../../include/sysconf.h:
+
+../../include/stdinc.h:
+
+../../libmowgli-2/src/libmowgli/mowgli.h:
+
+../../libmowgli-2/src/libmowgli/core/stdinc.h:
+
+../../libmowgli-2/src/libmowgli/platform/machine.h:
+
+../../libmowgli-2/src/libmowgli/platform/cacheline.h:
+
+../../libmowgli-2/src/libmowgli/platform/attributes.h:
+
+../../libmowgli-2/src/libmowgli/platform/constructor.h:
+
+../../libmowgli-2/src/libmowgli/core/logger.h:
+
+../../libmowgli-2/src/libmowgli/core/assert.h:
+
+../../libmowgli-2/src/libmowgli/core/iterator.h:
+
+../../libmowgli-2/src/libmowgli/container/list.h:
+
+../../libmowgli-2/src/libmowgli/object/class.h:
+
+../../libmowgli-2/src/libmowgli/object/object.h:
+
+../../libmowgli-2/src/libmowgli/core/allocation_policy.h:
+
+../../libmowgli-2/src/libmowgli/core/alloc.h:
+
+../../libmowgli-2/src/libmowgli/container/dictionary.h:
+
+../../libmowgli-2/src/libmowgli/thread/thread.h:
+
+../../libmowgli-2/src/libmowgli/thread/mutex.h:
+
+../../libmowgli-2/src/libmowgli/base/memslice.h:
+
+../../libmowgli-2/src/libmowgli/container/patricia.h:
+
+../../libmowgli-2/src/libmowgli/module/module.h:
+
+../../libmowgli-2/src/libmowgli/container/queue.h:
+
+../../libmowgli-2/src/libmowgli/base/hash.h:
+
+../../libmowgli-2/src/libmowgli/core/heap.h:
+
+../../libmowgli-2/src/libmowgli/core/bootstrap.h:
+
+../../libmowgli-2/src/libmowgli/base/bitvector.h:
+
+../../libmowgli-2/src/libmowgli/base/hook.h:
+
+../../libmowgli-2/src/libmowgli/base/mowgli_signal.h:
+
+../../libmowgli-2/src/libmowgli/ext/proctitle.h:
+
+../../libmowgli-2/src/libmowgli/ext/error_backtrace.h:
+
+../../libmowgli-2/src/libmowgli/base/random.h:
+
+../../libmowgli-2/src/libmowgli/base/argstack.h:
+
+../../libmowgli-2/src/libmowgli/object/message.h:
+
+../../libmowgli-2/src/libmowgli/object/metadata.h:
+
+../../libmowgli-2/src/libmowgli/ext/global_storage.h:
+
+../../libmowgli-2/src/libmowgli/core/process.h:
+
+../../libmowgli-2/src/libmowgli/eventloop/eventloop.h:
+
+../../libmowgli-2/src/libmowgli/vio/vio.h:
+
+../../libmowgli-2/src/libmowgli/core/mowgli_string.h:
+
+../../libmowgli-2/src/libmowgli/core/allocator.h:
+
+../../libmowgli-2/src/libmowgli/base/formatter.h:
+
+../../libmowgli-2/src/libmowgli/container/index.h:
+
+../../libmowgli-2/src/libmowgli/ext/confparse.h:
+
+../../libmowgli-2/src/libmowgli/ext/program_opts.h:
+
+../../libmowgli-2/src/libmowgli/ext/json.h:
+
+../../libmowgli-2/src/libmowgli/ext/json-inline.h:
+
+../../libmowgli-2/src/libmowgli/linebuf/linebuf.h:
+
+../../libmowgli-2/src/libmowgli/dns/dns.h:
+
+../../libmowgli-2/src/libmowgli/dns/evloop_res.h:
+
+../../libmowgli-2/src/libmowgli/dns/evloop_reslib.h:
+
+../../include/i18n.h:
+
+../../include/common.h:
+
+../../include/object.h:
+
+../../include/connection.h:
+
+../../include/res.h:
+
+../../include/atheme.h:
+
+../../include/reslib.h:
+
+../../include/hook.h:
+
+../../include/hooktypes.h:
+
+../../include/atheme_string.h:
+
+../../include/atheme_memory.h:
+
+../../include/table.h:
+
+../../include/servers.h:
+
+../../include/channels.h:
+
+../../include/module.h:
+
+../../include/privs.h:
+
+../../include/abirev.h:
+
+../../include/crypto.h:
+
+../../include/culture.h:
+
+../../include/base64.h:
+
+../../include/md5.h:
+
+../../include/sasl.h:
+
+../../include/match.h:
+
+../../include/account.h:
+
+../../include/entity.h:
+
+../../include/entity-validation.h:
+
+../../include/auth.h:
+
+../../include/tools.h:
+
+../../include/confprocess.h:
+
+../../include/global.h:
+
+../../include/flags.h:
+
+../../include/phandler.h:
+
+../../include/commandtree.h:
+
+../../include/servtree.h:
+
+../../include/services.h:
+
+../../include/users.h:
+
+../../include/sourceinfo.h:
+
+../../include/taint.h:
+
+../../include/database_backend.h:
+
+../../include/uid.h:
+
+../../include/inline/account.h:
+
+../../include/inline/channels.h:
+
+../../include/inline/connection.h:
+
+../../include/authcookie.h:
